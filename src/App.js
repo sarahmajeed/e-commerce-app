a7f90f04331b5338f5af510a7eb5f07c4d9e3fc4
Sarahmdawood <sarahmajeed368@gmail.com>
2020-05-13 15:31:03 +0500

    Setting up folders and router

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
 import React from 'react';
-import logo from './logo.svg';
+import { Route, Switch } from 'react-router-dom';
+import Navbar from './components/Navbar/Navbar';
+import ProductList from './components/ProductList/ProductList';
+import Details from './components/Details/Details';
+import Cart from './components/Cart/Cart';
+import Default from './components/Default/Default';
+
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <Navbar />
+      <Switch>
+        <Route path='/cart' component={Cart} />
+        <Route path='/details' component={Details} />
+        <Route path='/' component={ProductList} />
+        <Route component={Default} />
+      </Switch>
     </div>
   );
 }
